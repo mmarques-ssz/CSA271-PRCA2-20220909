b724956c9d0e296661727b126dd1f058b29d307c
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2022-09-09 20:26:03 -0300

    Add files via upload

---
+++ projAula0909/projAula0909/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projAula0909
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo t;
+            t = new Triangulo(3, 4, 5);
+
+            Console.WriteLine("Lado 1: {0}", t.getL1());
+            Console.WriteLine("Lado 2: {0}", t.getL2());
+            Console.WriteLine("Lado 3: {0}", t.getL3());
+
+            Console.WriteLine(t.ehRetangulo() ? 
+                "É triângulo retângulo" : 
+                "Não é triângulo retângulo");
+        }
+    }
+}
